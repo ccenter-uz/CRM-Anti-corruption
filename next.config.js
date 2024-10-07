9345383c7969163f8359b85a791776b95d586779
Muhammad <muxammad_205@mail.ru>
2024-10-07 18:32:59 +0500

    standalone

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withNextIntl = require("next-intl/plugin")();
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  output: 'standalone',
 };
 
 module.exports = withNextIntl(nextConfig);
